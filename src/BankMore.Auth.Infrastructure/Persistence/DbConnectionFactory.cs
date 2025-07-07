301735bd27b3f2fbab7de9e78ac3e5fa41766bdc
alexdevelopnet <alexdevelopnet@gmail.com>
2025-07-06 21:43:19 -0300

    ajuste no DbConnectionFactory

--- src/BankMore.Auth.Infrastructure/Persistence/DbConnectionFactory.cs
+++ src/BankMore.Auth.Infrastructure/Persistence/DbConnectionFactory.cs
@@ -1,29 +1,35 @@
 ﻿using BankMore.Auth.Domain.Abstractions;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
 using System.Data;
 
 namespace BankMore.Auth.Infrastructure.Persistence
 {
 
-    using Microsoft.Data.SqlClient;
-
     public class DbConnectionFactory : IDbConnectionFactory
     {
         private readonly IConfiguration _configuration;
-        private readonly string _connectionString;
+        private readonly bool _isDocker;
 
         public DbConnectionFactory(IConfiguration configuration)
         {
             _configuration = configuration;
-            _connectionString = _configuration.GetConnectionString("SqlServer");
+            _isDocker = Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") == "true";
         }
 
         public IDbConnection CreateConnection()
         {
-            return new SqlConnection(_connectionString);
+            if (_isDocker)
+            {
+                return new MySqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+            }
+            else
+            {
+                return new SqlConnection(_configuration.GetConnectionString("SqlServer"));
+            }
         }
     }
-
 
 
 }
